55dc140ef16e915a13c4ce3e380db87d54d371c0
Riste Jovanoski <riste.jovanoski@gmail.com>
2017-06-12 12:48:47 +0200

    [#8] Requested changes applied

--- self-service-registartion/src/main/java/com/msm/ssr/rest/controller/HealthCheckController.java
+++ self-service-registartion/src/main/java/com/msm/ssr/rest/controller/HealthCheckController.java
@@ -2,6 +2,8 @@ package com.msm.ssr.rest.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author riste.jovanoski
@@ -12,6 +14,7 @@ public class HealthCheckController {
 
     @GET
     @Path("/app-check")
+    @Produces(MediaType.TEXT_PLAIN)
     public String healthCheck() {
         return "The service self registration application is running";
     }
