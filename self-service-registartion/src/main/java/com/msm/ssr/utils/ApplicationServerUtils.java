33da610588850eef07919a6285300e62b37848a0
Riste Jovanoski <riste.jovanoski@gmail.com>
2017-06-14 13:54:24 +0200

    [#11] Applied requested changes

--- self-service-registartion/src/main/java/com/msm/ssr/utils/ApplicationServerUtils.java
+++ self-service-registartion/src/main/java/com/msm/ssr/utils/ApplicationServerUtils.java
@@ -16,6 +16,14 @@ import java.util.Set;
  */
 public class ApplicationServerUtils {
 
+    private static final MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+    private static final String CONNECTOR = "*:type=Connector,*";
+    private static final String PROTOCOL_LABEL = "protocol";
+    private static final String PROTOCOL_TYPE = "HTTP/1.1";
+    private static final String SCHEME_LABEL = "scheme";
+    private static final String PORT_LABEL = "port";
+
     /**
      * Gets the server url on which the application is running.
      * It returns a list with exactly two elements in which the first element is
@@ -28,41 +36,47 @@ public class ApplicationServerUtils {
      */
     public static List<String> getServerUrl(String contextPath) throws ApplicationServerUrlException {
         try {
-            InetAddress inetAddress = InetAddress.getLocalHost();
-            String host = inetAddress.getHostAddress();
-            String hostname = inetAddress.getHostName();
-            InetAddress[] addresses = InetAddress.getAllByName(hostname);
-
-            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-            Set<ObjectName> objects = mbs.queryNames(new ObjectName("*:type=Connector,*"), Query.match(Query.attr("protocol"), Query.value("HTTP/1.1")));
-            for (ObjectName obj : objects) {
-                String scheme = mbs.getAttribute(obj, "scheme").toString();
-                String port = obj.getKeyProperty("port");
-                for (InetAddress address : addresses) {
-                    String currentHost = address.getHostAddress();
-                    if (currentHost.equals(host)) {
-                        ArrayList<String> result = new ArrayList<String>();
-                        result.add(scheme + "://" + host + ":" + port + contextPath);
-                        result.add(scheme + "://" + hostname + ":" + port + contextPath);
-                        return result;
-                    }
+            Set<ObjectName> serverObjects = getServerObjects();
+            for (ObjectName serverObject : serverObjects) {
+                List<String> urls = constructServerUrlList(serverObject, contextPath);
+                if (!urls.isEmpty()) {
+                    return urls;
                 }
             }
-        } catch (MalformedObjectNameException ex) {
-            ex.printStackTrace();
-        } catch (UnknownHostException ex) {
-            ex.printStackTrace();
-        } catch (MBeanException ex) {
-            ex.printStackTrace();
-        } catch (AttributeNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (InstanceNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (ReflectionException ex) {
-            ex.printStackTrace();
+        } catch (MalformedObjectNameException | UnknownHostException | MBeanException | AttributeNotFoundException | InstanceNotFoundException | ReflectionException exception) {
+            exception.printStackTrace();
         }
 
         throw new ApplicationServerUrlException("Could not build server url");
     }
 
+    private static Set<ObjectName> getServerObjects() throws MalformedObjectNameException {
+        return mBeanServer.queryNames(new ObjectName(CONNECTOR), Query.match(Query.attr(PROTOCOL_LABEL), Query.value(PROTOCOL_TYPE)));
+    }
+
+    private static List<String> constructServerUrlList(ObjectName serverObject, String contextPath) throws AttributeNotFoundException, MBeanException, ReflectionException, InstanceNotFoundException, UnknownHostException {
+        String scheme = getScheme(serverObject);
+        String port = getPort(serverObject);
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String host = inetAddress.getHostAddress();
+        String hostname = inetAddress.getHostName();
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(buildServerUrl(scheme, host, port, contextPath));
+        result.add(buildServerUrl(scheme, hostname, port, contextPath));
+        return result;
+    }
+
+    private static String getScheme(ObjectName serverObject) throws AttributeNotFoundException, MBeanException, ReflectionException, InstanceNotFoundException {
+        return mBeanServer.getAttribute(serverObject, SCHEME_LABEL).toString();
+    }
+
+    private static String getPort(ObjectName serverObject) {
+        return serverObject.getKeyProperty(PORT_LABEL);
+    }
+
+    private static String buildServerUrl(String scheme, String host, String port, String contextPath) {
+        return String.format("%s://%s:%s%s", scheme, host, port, contextPath);
+    }
 }
