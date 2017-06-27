5110d1481564898b9ee56cde35b53fe2b38de02a
Riste Jovanoski <riste.jovanoski@gmail.com>
2017-06-27 13:53:40 +0200

    [#60] Service registrar code refactored according to code analysis

--- service-registrar/src/main/java/com/msm/sr/rest/actions/ServiceRegistrarActions.java
+++ service-registrar/src/main/java/com/msm/sr/rest/actions/ServiceRegistrarActions.java
@@ -4,6 +4,7 @@ package com.msm.sr.rest.actions;
  * @author riste.jovanoski
  * @since 6/20/2017
  */
+@SuppressWarnings("unused")
 public interface ServiceRegistrarActions {
 
     int register(String serviceId, String serviceHost, String healthEndpoint);
